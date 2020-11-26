db72d75931c836e5401ee87f65cb82c5bb488ba0
Vito Kortbeek <vito.kortbeek@gmail.com>
2020-11-26 11:33:26 +0100

    Change code-id location
    Code ID now either works using a random number defined by CMake
    (preferred) or a compile time unix time. But that one needs to be
    compiled at O2 (maybe O1) to be computed at compile time.

--- checkpoint/code-id/code_id.c
+++ checkpoint/code-id/code_id.c
@@ -6,22 +6,32 @@
  * We require the compiler to optimize this otherwise we waist quite a lot of
  * .text (flash)
  */
+#ifndef __clang__
 #pragma GCC push_options
 #pragma GCC optimize "-O2"
+#endif
 
 #include "code_id.h"
-#include "compile_time.h"
+
+#ifndef COMPILE_TIME_CODE_ID
+/* If no compile time code ID is provided, we attempt to make one */
+#include "compile-time/compile_time.h"
+#define COMPILE_TIME_CODE_ID UNIX_TIMESTAMP
+#else
+#endif
 
 code_id_t code_id_is_new(void) {
-  if (code_id != UNIX_TIMESTAMP) {
-    return UNIX_TIMESTAMP;
+  if (code_id != COMPILE_TIME_CODE_ID) {
+    return COMPILE_TIME_CODE_ID;
   }
   return 0;
 }
 
 code_id_t code_id_update(void) {
-    code_id = UNIX_TIMESTAMP;
+    code_id = COMPILE_TIME_CODE_ID;
     return code_id;
 }
 
+#ifndef __clang__
 #pragma GCC pop_options
+#endif
